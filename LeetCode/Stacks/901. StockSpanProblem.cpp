55e7e790add99fd92c929ba11fd7c83c3de7a26d
Nishant Joywardhan <njwardhan@gmail.com>
2022-11-10 02:27:24 +0530

    second approach to 901

--- LeetCode/Stacks/901. StockSpanProblem.cpp
+++ LeetCode/Stacks/901. StockSpanProblem.cpp
@@ -1,4 +1,10 @@
 // Nearest greator to the left: O(N) space-time solution
+// This below approach is basically using a stack and storing the relevant positions for further calculations.
+
+// One other approach could be to just use a simple array to go on storing answer values per price. That will ensure the inclusion of all the
+// lesser previous values, so for the current value, we can use the value of the last greator and so on .. --> DP approach
+
+// Both solutions will work in O(N) time and space
 
 class StockSpanner {
     stack<pair<int, int> > st;
