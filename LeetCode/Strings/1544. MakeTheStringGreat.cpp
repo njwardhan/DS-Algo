bab15784ceb0a62a7a6f61aa3849f913572d1636
Nishant Joywardhan <njwardhan@gmail.com>
2022-11-10 12:09:47 +0530

    clean and 4th approach to 1544

--- LeetCode/Strings/1544. MakeTheStringGreat.cpp
+++ LeetCode/Strings/1544. MakeTheStringGreat.cpp
@@ -1,17 +1,14 @@
-// ** Actually a stack question lol
-
 // 1. O(N^2) time, constant space solution
 
 class Solution {
 public:
     string makeGood(string s) {
-        int k, i = 0;
+        int i = 0;
         while(i < s.size()-1 and !s.empty())
         {
             if(tolower(s[i]) == tolower(s[i+1]) and int(s[i]) != int(s[i+1]))
             {
                 s.erase(i, 2);
-                k++;
                 i = 0;
             }
             else
@@ -43,6 +40,7 @@ public:
 };
 
 // 3. This is finally in O(N) time and space
+// Using a stack here can be a good way out
 
 class Solution {
 public:
@@ -72,5 +70,23 @@ public:
         
         reverse(answer.begin(), answer.end());
         return answer;
+    }
+};
+
+// 4. Good clean solution with O(N) time and space working
+
+class Solution {
+public:
+    string makeGood(string s) {
+        string result = "";
+        
+        for(char c : s)
+        {
+            if(result.length() and (tolower(result.back()) == tolower(c) and int(result.back()) != int(c)))
+               result.pop_back();
+            else
+               result.push_back(c);
+        }
+        return result;
     }
 };
